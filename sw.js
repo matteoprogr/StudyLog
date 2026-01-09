f76001f7e2914f4b0fca188c230403fe551de9da
matteoprogr <matteo93parisi@gmail.com>
2026-01-09 18:16:52 +0100

    prova estrazione materia da data onesignal

--- sw.js
+++ sw.js
@@ -105,11 +105,10 @@ self.addEventListener("push", (event) => {
   if (!event.data) return;
 
   const data = event.data.json();
-  const materia = payload?.custom?.a?.materia;
 
   event.waitUntil(
     self.registration.showNotification("Sessione completata", {
-      body: `Hai terminato lo studio di ${materia}`,
+      body: `Hai terminato lo studio di ${data}`,
       icon: "/icons/icon-192.png",
       badge: "/icons/icon-192.png",
       tag: "study-timer",
